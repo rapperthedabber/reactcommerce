25689c1f91aac9ee5a39218887d1f549658899fd
Jerry <jamesharold1738@gmail.com>
2023-05-17 09:04:09 -0400

    added edit profile feature with profile picture preview upon uploading and added editing icons

--- ecommerce/components/nav.js
+++ ecommerce/components/nav.js
@@ -21,10 +21,13 @@ export default function Nav(){
     const Profile =()=>{
         router.push('/Profile/Profile')
     }
+    const editProfile = () =>{
+        router.push('/editProfile/editProfile')
+    }
     const {pathname} = router;
     return(
     <div className= 'navBar'>
-        <div className={'flex float-right'}>
+        <div className={'flex'}>
         <svg  id = "tokyoIcon"xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
   <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
@@ -36,6 +39,7 @@ export default function Nav(){
         <button id='Products' onClick ={()=> handleProduct()}>Products</button>
         <button  id = "Profile" onClick = {()=> Profile()}>Profile</button>
         <Link href = {'/cart'}>Cart</Link>
+        <button id = 'editProfile' onClick={()=> editProfile()}>Edit Profile</button>
         
     </div>
     )}
--- ecommerce/pages/Profile/[...Profile].js
+++ ecommerce/pages/Profile/[...Profile].js
@@ -1,5 +1,6 @@
 
 import Nav from "@/components/nav"
+import Link from "next/link"
 export default function Profiler(){
     const Name = [{
         name: 'Jerry',
@@ -19,11 +20,17 @@ export default function Profiler(){
        
   
         <div className="ProfileCard">
+        <div className="penContainer">
+        <Link href = '/editProfile/editProfile'><svg   xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="pen"><path d="M21,12a1,1,0,0,0-1,1v6a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V5A1,1,0,0,1,5,4h6a1,1,0,0,0,0-2H5A3,3,0,0,0,2,5V19a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V13A1,1,0,0,0,21,12ZM6,12.76V17a1,1,0,0,0,1,1h4.24a1,1,0,0,0,.71-.29l6.92-6.93h0L21.71,8a1,1,0,0,0,0-1.42L17.47,2.29a1,1,0,0,0-1.42,0L13.23,5.12h0L6.29,12.05A1,1,0,0,0,6,12.76ZM16.76,4.41l2.83,2.83L18.17,8.66,15.34,5.83ZM8,13.17l5.93-5.93,2.83,2.83L10.83,16H8Z"></path></svg></Link>
         <img  id ="profileImage" src = "https://mn2s-content.s3.eu-west-2.amazonaws.com/wp-content/uploads/2023/03/24104727/Joey-The-Anime-Man.png" alt= 'no photo'></img>
+        </div>
         <span id ="profileName"> Name: </span>
+        <Link href = '/editProfile/editProfile'> <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="pen"><path d="M21,12a1,1,0,0,0-1,1v6a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V5A1,1,0,0,1,5,4h6a1,1,0,0,0,0-2H5A3,3,0,0,0,2,5V19a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V13A1,1,0,0,0,21,12ZM6,12.76V17a1,1,0,0,0,1,1h4.24a1,1,0,0,0,.71-.29l6.92-6.93h0L21.71,8a1,1,0,0,0,0-1.42L17.47,2.29a1,1,0,0,0-1.42,0L13.23,5.12h0L6.29,12.05A1,1,0,0,0,6,12.76ZM16.76,4.41l2.83,2.83L18.17,8.66,15.34,5.83ZM8,13.17l5.93-5.93,2.83,2.83L10.83,16H8Z"></path></svg></Link>
         <h2 id ='name'>{people.name}</h2>
+        <Link href = '/editProfile/editProfile' ><svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="pen"><path d="M21,12a1,1,0,0,0-1,1v6a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V5A1,1,0,0,1,5,4h6a1,1,0,0,0,0-2H5A3,3,0,0,0,2,5V19a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V13A1,1,0,0,0,21,12ZM6,12.76V17a1,1,0,0,0,1,1h4.24a1,1,0,0,0,.71-.29l6.92-6.93h0L21.71,8a1,1,0,0,0,0-1.42L17.47,2.29a1,1,0,0,0-1.42,0L13.23,5.12h0L6.29,12.05A1,1,0,0,0,6,12.76ZM16.76,4.41l2.83,2.83L18.17,8.66,15.34,5.83ZM8,13.17l5.93-5.93,2.83,2.83L10.83,16H8Z"></path></svg></Link>
         <span id ="profileName"> age: </span>
         <h2 id ='age'>{people.age}</h2>
+       <Link href ='/editProfile/editProfile'><svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="pen"><path d="M21,12a1,1,0,0,0-1,1v6a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V5A1,1,0,0,1,5,4h6a1,1,0,0,0,0-2H5A3,3,0,0,0,2,5V19a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V13A1,1,0,0,0,21,12ZM6,12.76V17a1,1,0,0,0,1,1h4.24a1,1,0,0,0,.71-.29l6.92-6.93h0L21.71,8a1,1,0,0,0,0-1.42L17.47,2.29a1,1,0,0,0-1.42,0L13.23,5.12h0L6.29,12.05A1,1,0,0,0,6,12.76ZM16.76,4.41l2.83,2.83L18.17,8.66,15.34,5.83ZM8,13.17l5.93-5.93,2.83,2.83L10.83,16H8Z"></path></svg></Link> 
         <span id ="profileBio"> bio: </span>
         <h2 id ='bio'>{people.bio}</h2>
         </div>
