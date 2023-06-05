f273bc2f3e44ffbca77a1ec6c813510721bc2efc
Jerry <jamesharold1738@gmail.com>
2023-06-05 14:45:00 -0400

    added node modules to git ignore

--- ecommerce/components/signOut.js
+++ ecommerce/components/signOut.js
@@ -4,9 +4,9 @@ import Link from "next/link";
 export default function Footer(){
     return(
         <footer>
-            <h1 id = 'footer-intro'>Tokyo Commerce</h1>
-            <h4 id ='footer-phrase'>An Otaku's paradise</h4>
-            <button><Link id = 'signOut' href={'/'}>Sign out</Link></button>
+            <h1 id = "footer-intro">Tokyo Commerce</h1>
+            <h4 id ="footer-phrase">An Otaku paradise</h4>
+            <button><Link id = "signOut" href={'/'}>Sign out</Link></button>
         </footer>
     )
 }
